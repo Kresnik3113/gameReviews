a0d4a7b20d71e2354f6240ec9fcb8a13d5fa80fd
Dean Kelly <18100287@student.ncirl.ie>
2021-07-28 16:00:26 +0100

    home controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :homes
   devise_for :users
-  root to: "home#index"
-  get '/about', to: 'home#about'
-  get '/contact', to: 'home#contact'
+  root to: "homes#index"
+  get '/about', to: 'homes#about'
+  get '/contact', to: 'homes#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
