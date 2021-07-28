74b05ac82094ed2b4672227ff1a1b303b9152bbd
Dean Kelly <18100287@student.ncirl.ie>
2021-07-28 17:49:21 +0100

    nav update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
- protect_from_forgery with: :exception
- before_action :authenticate_user!
+    protect_from_forgery with: :exception
+    #before_action :authenticate_user!
 
 def after_sign_in_path_for(resource)
 "/signedinuserprofile" end
