aab5d7411fc8212e4e7f57427e49a195df2cd556
Dean Kelly <18100287@student.ncirl.ie>
2021-07-30 00:06:31 +0100

    final push

--- db/migrate/20210728163022_update_users.rb
+++ db/migrate/20210728163022_update_users.rb
@@ -1,4 +1,5 @@
 class UpdateUsers < ActiveRecord::Migration[4.2]
+  user=User.create!(:email=>'admin@email.ie',:password=>'password')
   def change
     @u = User.find_by( email: 'admin@email.ie' )
     @u.update_attribute :admin, true
